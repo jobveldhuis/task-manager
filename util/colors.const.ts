ca07bac4e91a2e4eba9b82fab75f0c34d03c267b
Veldhuis <jobbfveldhuis@gmail.com>
2023-08-22 17:27:34 +0200

    feat: add primary and secondary colors

--- util/colors.const.ts
+++ util/colors.const.ts
@@ -1,21 +1,7 @@
-const tintColorLight = "#2f95dc";
-const tintColorDark = "#fff";
-
 export const COLORS = {
-  light: {
-    text: "#000",
-    background: "#fff",
-    tint: tintColorLight,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: "#fff",
-    background: "#000",
-    tint: tintColorDark,
-    tabIconDefault: "#ccc",
-    tabIconSelected: tintColorDark,
-  },
+  primary: "#343C64",
+  secondary: "#000",
+  inverse: "#fff",
   backgroundGradient: {
     primary: "#E56420",
     secondary: "#37BADE",
