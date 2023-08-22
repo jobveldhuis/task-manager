9a8c6c39f83bfc8548844b0a171147127b5c9d05
Veldhuis <jobbfveldhuis@gmail.com>
2023-08-22 18:21:04 +0200

    feat: add button colors

--- util/colors.const.ts
+++ util/colors.const.ts
@@ -2,6 +2,10 @@ export const COLORS = {
   primary: "#343C64",
   secondary: "#000",
   inverse: "#fff",
+  button: {
+    primary: "#343C64",
+    inverse: "#fff",
+  },
   backgroundGradient: {
     primary: "#E56420",
     secondary: "#37BADE",
