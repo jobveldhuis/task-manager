de5fcdf8b528b591ae739e281c4b658cdaad65f5
Veldhuis <jobbfveldhuis@gmail.com>
2023-08-23 13:35:08 +0200

    fix: remove icon where it is not supposed to be

--- components/tutorial/introduction.tsx
+++ components/tutorial/introduction.tsx
@@ -5,9 +5,7 @@ import { Title } from "@/ui/title";
 export function TutorialIntroductionScreen(): JSX.Element {
   return (
     <BackgroundView>
-      <Title style={{ marginBottom: 16 }} icon="calendar-alt">
-        Before we start
-      </Title>
+      <Title style={{ marginBottom: 16 }}>Before we start</Title>
       <Text variant="blackParagraph" style={{ marginBottom: 16 }}>
         We would like to tell you a bit about how this journey will look. Don't
         worry, it only takes about 2 minutes.
