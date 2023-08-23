795531786f98b6b361bf29832428af2bd969c93b
Veldhuis <jobbfveldhuis@gmail.com>
2023-08-23 14:00:17 +0200

    feat: finish tutorial

--- components/tutorial/introduction.tsx
+++ components/tutorial/introduction.tsx
@@ -6,11 +6,11 @@ export function TutorialIntroductionScreen(): JSX.Element {
   return (
     <BackgroundView>
       <Title style={{ marginBottom: 16 }}>Before we start</Title>
-      <Text variant="blackParagraph" style={{ marginBottom: 16 }}>
+      <Text style={{ marginBottom: 16 }}>
         We would like to tell you a bit about how this journey will look. Don't
         worry, it only takes about 2 minutes.
       </Text>
-      <Text variant="blackParagraph">
+      <Text>
         To move between screens, swipe from left to right and right to left.
       </Text>
     </BackgroundView>
--- components/tutorial/what-it-is.tsx
+++ components/tutorial/what-it-is.tsx
@@ -1,5 +1,47 @@
 import { BackgroundView } from "@/ui/background-view";
+import { TutorialItem } from "@/components/tutorial/tutorial-item";
+import { Separator } from "@/ui/separator";
+import { StyleSheet } from "react-native";
 
 export function TutorialWhatItIsScreen(): JSX.Element {
-  return <BackgroundView></BackgroundView>;
+  return (
+    <BackgroundView>
+      <TutorialItem title="It's a list" icon="list" style={styles.item}>
+        Nothing revolutionary here. We store the tasks you want to accomplish,
+        you decide when you do then.
+      </TutorialItem>
+      <Separator style={styles.separator} />
+      <TutorialItem title="3 simple questions" icon="edit" style={styles.item}>
+        Every time you want to add something to your to-do list, we need you to
+        answer three basic questions.
+      </TutorialItem>
+      <Separator style={styles.separator} />
+      <TutorialItem
+        title="Check it off"
+        icon="check-double"
+        style={styles.item}
+      >
+        Completed a task? When you check it off your list, we ask for a little
+        bit of feedback, using emoji.
+      </TutorialItem>
+      <Separator style={styles.separator} />
+      <TutorialItem
+        title="That's literally it"
+        icon="flag-checkered"
+        style={styles.item}
+      >
+        Sounds boring, right? Well, getting sh*t done can be a bit boring, but
+        also very, very rewarding.
+      </TutorialItem>
+    </BackgroundView>
+  );
 }
+
+const styles = StyleSheet.create({
+  separator: {
+    marginBottom: 24,
+  },
+  item: {
+    marginBottom: 24,
+  },
+});
