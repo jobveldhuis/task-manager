d0cb18ab73f2f0afcff892aeb3a32b779564e789
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-05 12:20:17 +0200

    chore: properly use file naming to identify type in file

--- backend/authentication/authentication.constant.ts
+++ backend/authentication/authentication.constant.ts
@@ -1,4 +1,8 @@
-import { getAuth } from "@firebase/auth";
+import { getAuth, getReactNativePersistence } from "@firebase/auth";
 import { app } from "../app.constant";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 export const authentication = getAuth(app);
+authentication.setPersistence(
+  getReactNativePersistence(ReactNativeAsyncStorage),
+);
