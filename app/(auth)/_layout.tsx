3a7bb873593a6250a5dec2ea5bec7327824a1615
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-05 12:21:48 +0200

    feat: if user is already authenticated, don't show the authentication routes

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,16 @@
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
+import { useAuthentication } from "@/backend/authentication";
+import { useEffect } from "react";
 
 export default function AuthLayout() {
+  const { isAuthenticated } = useAuthentication();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push("/(app)/main");
+    }
+  }, []);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
