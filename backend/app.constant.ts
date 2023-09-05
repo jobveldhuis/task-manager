a126f60466e6387130363aca1fae410f9cf59eb2
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-05 11:09:10 +0200

    feat: initialize firebase using `.env` variables

---
+++ backend/app.constant.ts
@@ -0,0 +1,14 @@
+import type { FirebaseOptions } from "@firebase/app";
+import { initializeApp } from "@firebase/app";
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
+};
+
+export const app = initializeApp(firebaseConfig);
