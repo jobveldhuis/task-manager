5b4bcfee9cc4fb988083a43f40847832596f9d1d
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-13 17:05:12 +0200

    feat: make safe storage user data more flexible for future use

--- util/has-seen-tutorial.ts
+++ util/has-seen-tutorial.ts
@@ -1,10 +1,12 @@
 import { getItemAsync } from "expo-secure-store";
 
 export async function hasSeenTutorial(userId: string): Promise<boolean> {
-  const hasSeenTutorial = await getItemAsync(userId);
+  const userData = await getItemAsync(userId);
 
-  // Since SecureStore stores in key/value pairs of strings, we should check
-  // for a literal string true. Any other return means the user has not yet
-  // seen the tutorial on this device.
-  return hasSeenTutorial === "true";
+  if (userData === null) {
+    return false;
+  }
+
+  const data = JSON.parse(userData);
+  return data.hasSeenTutorial === true;
 }
--- util/mark-tutorial-as-seen.ts
+++ util/mark-tutorial-as-seen.ts
@@ -1,5 +1,12 @@
-import { setItemAsync } from "expo-secure-store";
+import { getItemAsync, setItemAsync } from "expo-secure-store";
 
-export function markTutorialAsSeen(userId: string): Promise<void> {
-  return setItemAsync(userId, "true");
+export async function markTutorialAsSeen(userId: string): Promise<void> {
+  const storedData = await getItemAsync(userId);
+  const currentData = JSON.parse(storedData ?? "{}");
+  const data = JSON.stringify({
+    ...currentData,
+    hasSeenTutorial: true,
+  });
+
+  return setItemAsync(userId, data);
 }
