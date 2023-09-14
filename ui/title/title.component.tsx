d61ca298f894d9d7b91f4d8c56cac0691ea39145
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-14 14:18:24 +0200

    feat: allow border for title component

--- ui/title/title.component.tsx
+++ ui/title/title.component.tsx
@@ -3,31 +3,38 @@ import { Text } from "react-native";
 import { FilteredStyleProps } from "@/util/filtered-style-props.type";
 import { ReactNode } from "react";
 import { IconName, Icon } from "@/ui/icon";
+import { COLORS } from "@/util/colors.const";
 
 type TitleProps = {
   children: ReactNode;
   icon?: IconName;
   variant?: (typeof variants)[number];
   style?: FilteredStyleProps<TextStyle> | FilteredStyleProps<ViewStyle>;
+  hasBorder?: boolean;
 };
 
 export const variants = ["default", "inline"] as const;
 
 export function Title({
-  variant = "default",
   style,
   children,
   icon,
+  variant = "default",
+  hasBorder = false,
 }: TitleProps): JSX.Element {
   const hasIcon = icon !== undefined;
 
-  return hasIcon ? (
-    <View style={[styles.container, style]} testID="title-container">
-      <Icon name={icon} />
+  // A border can only occur on the default title component
+  const containerStyles =
+    hasBorder && variant === "default"
+      ? [styles.container, styles.border, style]
+      : [styles.container, style];
+
+  return (
+    <View style={containerStyles} testID="title-container">
+      {hasIcon && <Icon name={icon} />}
       <Text style={styles[variant]}>{children}</Text>
     </View>
-  ) : (
-    <Text style={[styles[variant], style]}>{children}</Text>
   );
 }
 
@@ -41,10 +48,17 @@ const styles = StyleSheet.create({
   default: {
     fontSize: 24,
     fontWeight: "bold",
+    color: COLORS.primary,
+  },
+  border: {
+    width: "100%",
+    borderBottomColor: COLORS.primary,
+    borderBottomWidth: 1,
   },
   inline: {
     fontSize: 16,
     fontWeight: "bold",
     marginBottom: 24,
+    color: COLORS.secondary,
   },
 });
