26878d0107fe626bf6bc1cae50c72f66e6711120
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-14 14:18:11 +0200

    chore: allow `paddingBottom` style prop

--- util/filtered-style-props.type.ts
+++ util/filtered-style-props.type.ts
@@ -1,5 +1,5 @@
 import { ViewStyle } from "react-native";
 
-type PickedProps = "marginBottom";
+type PickedProps = "marginBottom" | "paddingBottom";
 
 export type FilteredStyleProps<T extends ViewStyle> = Pick<T, PickedProps>;
