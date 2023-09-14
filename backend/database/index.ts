8c22188fa6ab4802e29cf1c4fec199ff45df526b
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-14 18:07:05 +0200

    chore: export from `backend/database`

---
+++ backend/database/index.ts
@@ -0,0 +1,4 @@
+export { database } from "./database";
+export { getTodosByUser } from "./get-todos-by-user";
+export { setCompletedStatusOnTodo } from "./set-completed-status-on-todo";
+export { createTodo } from "./create-todo";
