890628b7bc75fa46ba18ca25fd1ec34ff9da90f2
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-14 18:08:13 +0200

    feat: render TODOs

--- components/todos/todo-list.component.tsx
+++ components/todos/todo-list.component.tsx
@@ -1,14 +1,32 @@
 import { Page } from "../app/page.component";
+import { getTodosByUser } from "@/backend/database/get-todos-by-user";
+import { useAuthentication } from "@/backend/authentication";
+import { useEffect, useState } from "react";
+import { Todo } from "@/types/todo.type";
 import { TodoItem } from "@/components/todos/todo-item.component";
+import { FullPageSpinner } from "@/ui/full-page-spinner";
 
 export function TodoList(): JSX.Element {
-  const todos = [];
+  const { user } = useAuthentication();
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (user === null) return;
+
+    getTodosByUser(user.uid).then((result) => setTodos(result));
+    setIsLoading(false);
+  }, [user]);
 
   return (
     <Page title="Things you could pick up">
-      {todos.map((item) => (
-        <TodoItem isChecked={false} title={item.title} />
-      ))}
+      {isLoading ? (
+        <FullPageSpinner />
+      ) : (
+        todos.map(({ id, title, isCompleted }) => (
+          <TodoItem key={id} id={id} isChecked={isCompleted} title={title} />
+        ))
+      )}
     </Page>
   );
 }
