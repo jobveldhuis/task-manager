4f0bb5f32ac4fdf9416b651a9aaa72543b7b66ca
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-20 16:18:02 +0200

    fix: correct import from `index`

--- backend/database/index.ts
+++ backend/database/index.ts
@@ -1,4 +1,4 @@
 export { database } from "./database";
 export { getTodosByUser } from "./get-todos-by-user";
-export { setCompletedStatusOnTodo } from "./set-completed-status-on-todo";
+export { setCompletedStatus } from "./set-completed-status";
 export { createTodo } from "./create-todo";
