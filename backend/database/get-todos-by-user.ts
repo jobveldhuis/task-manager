f6a7165004806cda8e2ee4695ebe5c4e91a59381
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-20 16:17:52 +0200

    feat: add `createdAt` when fetching a todo

--- backend/database/get-todos-by-user.ts
+++ backend/database/get-todos-by-user.ts
@@ -3,19 +3,30 @@ import { database } from "@/backend/database/database";
 import {
   collection,
   getDocs,
+  or,
   query,
   QueryDocumentSnapshot,
+  where,
 } from "@firebase/firestore";
+import { getStartOfDay } from "@/util/get-start-of-day";
 
 function mapResultToTodo(result: QueryDocumentSnapshot): Todo {
   const { id } = result;
 
-  const { title, description, completionDate, expectedCompletionDate, rating } =
-    result.data();
+  const {
+    title,
+    description,
+    completionDate,
+    expectedCompletionDate,
+    rating,
+    createdAt,
+  } = result.data();
 
   const mappedExpectedCompletionDate = new Date(
     expectedCompletionDate.seconds * 1000,
   );
+
+  const mappedCreatedAt = new Date(createdAt.seconds * 1000);
 
   const isCompleted = completionDate != null;
 
@@ -23,12 +34,15 @@ function mapResultToTodo(result: QueryDocumentSnapshot): Todo {
     id,
     title,
     description,
+    createdAt: mappedCreatedAt,
     expectedCompletionDate: mappedExpectedCompletionDate,
   };
 
   if (!isCompleted) {
     return {
       ...baseTodo,
+      rating: null,
+      completionDate: null,
       isCompleted,
     };
   }
@@ -36,11 +50,8 @@ function mapResultToTodo(result: QueryDocumentSnapshot): Todo {
   const mappedCompletionDate = new Date(completionDate.seconds * 1000);
 
   return {
-    id,
-    title,
-    description,
+    ...baseTodo,
     rating,
-    expectedCompletionDate: mappedExpectedCompletionDate,
     completionDate: mappedCompletionDate,
     isCompleted,
   };
@@ -48,7 +59,15 @@ function mapResultToTodo(result: QueryDocumentSnapshot): Todo {
 
 export async function getTodosByUser(userId: string): Promise<Todo[]> {
   const reference = collection(database, "users", userId, "todos");
-  const q = query(reference);
+  const startOfDay = getStartOfDay(new Date());
+
+  const q = query(
+    reference,
+    or(
+      where("completionDate", "==", null),
+      where("completionDate", ">=", startOfDay),
+    ),
+  );
   const snapshot = await getDocs(q);
   return snapshot.docs.map(mapResultToTodo);
 }
