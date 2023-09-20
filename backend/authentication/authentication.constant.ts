825f442eeab2f85d826991bfc60ca2742b9162b4
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-20 16:15:59 +0200

    chore: add `@ts-ignore` directive for existing import

--- backend/authentication/authentication.constant.ts
+++ backend/authentication/authentication.constant.ts
@@ -1,3 +1,5 @@
+// REASON: it does exist - https://firebase.google.com/docs/reference/js/auth.md#getreactnativepersistence
+// @ts-ignore
 import { getReactNativePersistence, initializeAuth } from "@firebase/auth";
 import { app } from "../app.constant";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
