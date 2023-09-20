4986244978edca8d8358bbac81db05332d8bd3ef
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-21 01:34:05 +0200

    chore: add export `index` files

---
+++ src/components/app/index.tsx
@@ -0,0 +1,5 @@
+export { AppPagination } from "./app-pagination.component";
+export { Page } from "./page.component";
+export { SettingsPage } from "./settings-page.component";
+export { TodoPage } from "./todo-page.component";
+export { CreateTodoPage } from "./create-todo-page.component";
