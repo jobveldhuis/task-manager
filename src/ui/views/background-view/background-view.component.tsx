b711fab18e59fbbe9ada4ee98a4d3a554f317883
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-22 14:32:27 +0200

    chore: correct import

--- src/ui/views/background-view/background-view.component.tsx
+++ src/ui/views/background-view/background-view.component.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { StyleSheet } from "react-native";
-import { LinearGradient } from "../../linear-gradient";
+import { LinearGradient } from "@/ui/linear-gradient";
 
 type BackgroundViewProps = {
   children: ReactNode;
