746bb2f1291f85ffddad437d44bd20a6bf39b34c
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-22 14:31:41 +0200

    feat: export types directly from `@/types`

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type { Rating } from "./rating.type";
+export { ratingValues } from "./rating.type";
+export type { Todo, CompletedTodo, UnfinishedTodo } from "./todo.type";
+export type { UserStatistics } from "./user-statistics.type";
