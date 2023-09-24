ec2a71a822782523d698790a26c7ddb276e3b9af
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-24 15:51:04 +0200

    feat: add `onLongPress` event for todo items

--- src/components/todos/todo-item.component.tsx
+++ src/components/todos/todo-item.component.tsx
@@ -1,5 +1,6 @@
 import {
   GestureResponderEvent,
+  Pressable,
   StyleSheet,
   View,
   ViewStyle,
@@ -13,19 +14,28 @@ type TodoItemProps = {
   data: Todo;
   style?: FilteredStyleProps<ViewStyle>;
   onPress: (event: GestureResponderEvent) => void;
+  onLongPress: (event: GestureResponderEvent) => void;
 };
 
 export function TodoItem({
   onPress: handlePress,
+  onLongPress: handleLongPress,
   style,
   data,
 }: TodoItemProps): JSX.Element {
   const { isCompleted, title } = data;
-
   return (
     <View style={[styles.container, style]}>
-      <Checkbox isChecked={isCompleted} onPress={handlePress} />
-      <Text variant={isCompleted ? "strikethrough" : "paragraph"}>{title}</Text>
+      <Checkbox
+        isChecked={isCompleted}
+        onPress={handlePress}
+        onLongPress={handleLongPress}
+      />
+      <Pressable onPress={handlePress} onLongPress={handleLongPress}>
+        <Text variant={isCompleted ? "strikethrough" : "paragraph"}>
+          {title}
+        </Text>
+      </Pressable>
     </View>
   );
 }
