61e1d64f08aa42a8acdc2f90a52ae24ad86ae24e
Veldhuis <jobbfveldhuis@gmail.com>
2023-09-24 15:50:54 +0200

    feat: export all backend functions

--- src/backend/database/index.ts
+++ src/backend/database/index.ts
@@ -2,3 +2,6 @@ export { database } from "./database";
 export { getTodosByUser } from "./get-todos-by-user";
 export { setCompletedStatus } from "./set-completed-status";
 export { createTodo } from "./create-todo";
+export { getStatisticsByUser } from "./get-statistics-by-user";
+export { deleteTodo } from "./delete-todo";
+export { setRating } from "./set-rating";
